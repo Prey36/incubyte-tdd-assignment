90456dc4695bef874703b323ddfac201af7eeb60
Prey36 <preydalsaniya@gmail.com>
2024-07-30 01:40:08 +0530

    First commit for test

---
+++ test_case_1.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int add(string input){
+    return 0;
+}
+
+int main(){
+    string input;
+    cin >> input;
+    int output = add(input);
+    cout << output << endl;
+    
+}
